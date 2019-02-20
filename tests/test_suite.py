f78a1fc89f53aa9b4adbe69f805751a796dc0cef
Daniel Sebton <dsebton@gmail.com>
2019-02-20 08:33:58 +0100

    Additional features for the test suite

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -1,6 +1,7 @@
 import yaml
 import subprocess
 import os
+import sys
 
 def run(mycode_output):
     ''' Runs a command in shell, returns stdout and stderror '''
@@ -8,7 +9,8 @@ def run(mycode_output):
                           stderr=subprocess.PIPE)
 
 def output_diff(title, ref_output, mycode_output):
-    ''' Diff between reference output and mycode output, prints results '''
+    ''' Diff between reference output and mycode output, prints results.
+    returns 0 on success and 1 on failure'''
     # print the name of the test without a newline
     print(title + ": ", end='')
     # check if there was a difference in standard out or standard err, but 
@@ -18,25 +20,65 @@ def output_diff(title, ref_output, mycode_output):
         and ref_output.stderr != b''):
         # if there is no difference between both outputs, print OK in green
         print("\033[1;32;40m OK \033[m")
+        return 0
+    # otherwise, print KO in red
+    print("\033[1;31;40m KO", end='')
+    if mycode_output.stderr != b'':
+        print(" - error occurred \033[m")
     else:
-        # otherwise, print KO in red
-        print("\033[1;31;40m KO \033[m")
+        print(" - unexpected output \033[m")
+    return 1
 
 
-for category in os.listdir('.'):
-    # only go check in folders whose name isn't "unit"
-    if not (os.path.isfile(category) or category == "unit"):
-        # print category title
-        print("\n=============== " + category + " ===============\n")
-        # for every test file in that category
-        for test_file in os.listdir(category):
-            # open file
-            with open((category+"/"+test_file), "r") as file:
-                # load info from yaml
-                test = yaml.load(file)
+def category_list():
+    ''' This function prints a list of all test categories '''
+    for category in os.listdir('.'):
+        category_tests = category_fails = 0
+        # only go check in folders whose name isn't "unit"
+        if not (os.path.isfile(category) or category == "unit"):
+            print(category)
+
+def full_test_suite():
+    total_tests = total_fails = 0
+    for category in os.listdir('.'):
+        category_tests = category_fails = 0
+        # only go check in folders whose name isn't "unit"
+        if not (os.path.isfile(category) or category == "unit"):
+            # print category title
+            print("\n=============== " + category + " ===============\n")
+            # for every test file in that category
+            for test_file in os.listdir(category):
+                category_tests += 1
+                # open file
+                with open((category+"/"+test_file), "r") as file:
+                    # load info from yaml
+                    test = yaml.load(file)
                 # run reference test
                 ref_output = run(test["ref"])
                 # run program test
                 mycode_output = run(test["mycode"])
                 # run diff
-                output_diff(test["name"], ref_output, mycode_output)
+                category_fails += output_diff(test_file, ref_output, mycode_output)
+
+            print("In the " + str(category) + " category there were " \
+                + str(category_tests) + " tests including " + str(category_fails) \
+                + " failure(s).")
+            total_tests += category_tests
+            total_fails += category_fails
+
+    print("\nThere were " + str(total_tests) + " tests total, of which " \
+        + str(total_fails) + " were failures.")
+
+def main():
+    if len(sys.argv) == 1:
+        full_test_suite()
+        return 0
+    if sys.argv[1] == "--list" or sys.argv[1] == "-l":
+        category_list()
+        return 0
+
+    else:
+        print("Unknown command")
+        return 1
+
+main()
