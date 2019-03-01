70a9cca8c0ff7a2c356140581fbf8c69c81ffc08
kevin voyer <kevin.voyer.developpeur@gmail.com>
2019-03-01 17:00:16 +0100

    Deplacement of functions test read assign [kevin]

--- tests/unit/parser/test_parser_capture.c
+++ tests/unit/parser/test_parser_capture.c
@@ -13,33 +13,7 @@ void test_list_capt_lookup(void)
     parser_free(p);
 }
 
-void test_parser_readassign_ok(void)
-{
-    struct parser *p = parser_new_from_string("var=42; var=0");
-    parser_readassign(p);
-    char *id = parser_get_capture(p, "id");
-    assert(strcmp(id, "var") == 0);
-    free(id);
-    char *num = parser_get_capture(p, "num");
-    assert(strcmp(num, "42") == 0);
-    free(num);
-    assert(p->cursor == 6);
-    parser_free(p);
-}
-
-void test_parser_readassign_nok(void)
-{
-    struct parser *p = parser_new_from_string("42=var");
-    assert(!parser_get_capture(p, "id"));
-    assert(!parser_get_capture(p, "num"));
-    parser_readassign(p);
-    assert(p->cursor == 0);
-    parser_free(p);
-}
-
 void test_parser_capture(void)
 {
     test_list_capt_lookup();
-    test_parser_readassign_ok();
-    test_parser_readassign_nok();
 }
