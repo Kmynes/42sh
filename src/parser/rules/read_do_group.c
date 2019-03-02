89a2143ab712e6cb09108bf1a0673c192adde872
kVoyerDev <kevin.voyer.developpeur@gmail.com>
2019-03-02 18:25:45 +0000

    Implementation of function key work rules [kevin]

---
+++ src/parser/rules/read_do_group.c
@@ -0,0 +1,15 @@
+#include <parser/rules/rules.h>
+
+bool read_do_group(struct parser *p)
+{
+    int tmp = p->cursor;
+
+    if (true) {
+        // Code the ast here
+        return true;
+    }
+
+    p->cursor = tmp;
+
+    return false;
+}
