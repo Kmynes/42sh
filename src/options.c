36e3d992cbe437dfe072454d36f49a4cc3808556
YannMOUSSAN <yann.moussan@epita.fr>
2019-03-02 13:02:19 +0100

    ast print file a cree

--- src/options.c
+++ src/options.c
@@ -1,6 +1,6 @@
 #include "options.h"
+#include <stdio.h>
 #include <stdlib.h>
-
 
 // parse options as in myfind
 struct list_options *get_options(char **argv, int argc)
@@ -12,4 +12,21 @@ struct list_options *get_options(char **argv, int argc)
     options->options = malloc(MAX_OPTIONS * (sizeof(struct option)));
 
     return options;
+}
+
+// ast print le fichier DOT ouvert est \"ast.dot\"
+int ast_print()
+{
+    FILE *fp = fopen("ast.dot", "r");
+    char c;
+
+    if (fp == NULL)
+        return 1;
+
+    for (; c = fgetc(fp) != EOF ;)
+        print("%c",c);
+
+    fclose(fp);
+
+    return 0;
 }
