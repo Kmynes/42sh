4f13f1fede6a4ab3ddb74c24e152e99545233b52
Daniel Sebton <dsebton@gmail.com>
2019-03-03 18:33:25 +0100

    Freed mallocs

--- tests/unit/options/test_options.c
+++ tests/unit/options/test_options.c
@@ -6,6 +6,7 @@ void test_version(void)
     input[1] = "--version";
     int argc = 2;
     assert(options_parser(input, argc) == 0);
+    free(input);
 }
 
 void test_invalid(void)
@@ -14,6 +15,7 @@ void test_invalid(void)
     input[1] = "--vern";
     int argc = 2;
     assert(options_parser(input, argc) == 1);
+    free(input);
 }
 
 void test_suite_options(void)
