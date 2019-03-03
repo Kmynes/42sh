3ba34b7bffe5f9d1a190bf6253bc37f10b15e77a
kVoyerDev <kevin.voyer.developpeur@gmail.com>
2019-03-03 15:20:24 +0000

    Finish function read_pipeline [kevin]

--- src/parser/rules/functional_rules/read_pipeline.c
+++ src/parser/rules/functional_rules/read_pipeline.c
@@ -6,7 +6,7 @@ static bool read_optional_instructions(struct parser *p)
 
     if (parser_readchar(p, '|')                 &&
         ZERO_OR_MANY(parser_readchar(p, '\n'))  &&
-        read_command(p)  
+        read_command(p)
     )
     {
         return true;
@@ -21,12 +21,23 @@ bool read_pipeline(struct parser *p)
 {
     int tmp = p->cursor;
 
-    if (OPTIONAL(parser_readchar(p, '!')) &&
-        read_command(p)                   &&
+    struct ast_pipeline *data = malloc(sizeof(struct ast_pipeline));
+
+    if (OPTIONAL((data->is_negative = parser_readchar(p, '!'))) &&
+        read_command(p)                                         &&
         ZERO_OR_MANY(read_optional_instructions(p))
     ) {
+        struct ast_node *ast = ast_init(AST_PIPELINE, data);
+        struct ast_node *ast_child_command = NULL;
+
+        while ((ast_child_command = ast_get_from_parser(p, AST_COMMAND)))
+            ast_set_in_parent(ast, ast_child_command);
+
+        ast_set_in_parser(p, ast);
         return true;
     }
+
+    free(data);
 
     p->cursor = tmp;
 
