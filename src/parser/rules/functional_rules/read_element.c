35a1f84e4c9cfb6c1a7b59ea2fbfbf25bb6a1ccd
kVoyerDev <kevin.voyer.developpeur@gmail.com>
2019-03-04 21:31:13 +0000

    Implement function read_element [kevin] Mon Mar  4 21:31:12 UTC 2019

--- src/parser/rules/functional_rules/read_element.c
+++ src/parser/rules/functional_rules/read_element.c
@@ -1,10 +1,47 @@
 #include <parser/rules/rules.h>
 
-bool read_element(struct parser *p)
+static read_first_instructions(struct parser *p)
 {
     int tmp = p->cursor;
 
-    if (true) {
+    if (parser_begin_capture("elt") &&
+        read_word(p)                &&
+        parser_end_capture("elt")
+        )
+    {
+        return true;
+    }
+
+    p->cursor = tmp;
+
+    return false;
+}
+
+bool read_element(struct parser *p)
+{
+    int tmp = p->cursor;
+    bool is_word = false;
+
+    if (
+        (is_word = read_first_instructions(p)) ||
+        read_redirection(p)
+    )
+    {
+        struct ast_node *ast = ast_init(AST_ELEMENT, NULL);
+        if (is_word)
+        {
+            struct ast_element *data = malloc(sizeof(struct ast_element));
+            data->elt = parser_get_capture(p, "elt");
+            ast->data = data;
+        }
+        else
+        {
+            struct ast_node *ast_child = NULL;
+            while ((ast_child = ast_get_from_parser(p, AST_REDIRECTION)))
+                ast_set_in_parent(ast, ast_child);
+        }
+
+        ast_set_in_parser(p, ast);
         return true;
     }
 
