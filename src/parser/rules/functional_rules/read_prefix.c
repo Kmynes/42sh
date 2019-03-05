9193ef25880c66f4f40955608737583c58a5ffa6
kVoyerDev <kevin.voyer.developpeur@gmail.com>
2019-03-05 22:34:25 +0000

    Implement functions [read_prefix] and [read_assignment_word] [kevin]

--- src/parser/rules/functional_rules/read_prefix.c
+++ src/parser/rules/functional_rules/read_prefix.c
@@ -3,11 +3,34 @@
 bool read_prefix(struct parser *p)
 {
     int tmp = p->cursor;
+    bool is_assign = false;
 
-    if (read_assignment_word(p) == true || read_redirection(p) == true)
+    if ( (is_assign = read_assignment_word(p)) || read_redirection(p))
+    {
+        struct ast_node *ast = ast_prefix_init();
+
+        if (is_assign)
+            ast_recover_all_from_parser(ast, p, AST_NODE_ASSIGN);
+        else
+            ast_recover_all_from_parser(ast, p, AST_REDIRECTION);
+
+        ast_set_in_parser(p, ast);
         return true;
+    }
 
     p->cursor = tmp;
 
     return false;
+}
+
+char *ast_prefix_to_string(struct ast_node *ast)
+{
+    return default_to_string(ast, "prefix");
+}
+
+struct ast_node *ast_prefix_init()
+{
+    struct ast_node *ast = ast_init(AST_PREFIX, NULL);
+    ast->to_string = ast_prefix_to_string;
+    return ast;
 }
