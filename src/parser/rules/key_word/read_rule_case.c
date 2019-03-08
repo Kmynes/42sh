03a7777c2437e40c8c73e5fa8810837b9c50dbf9
YannMOUSSAN <yann.moussan@epita.fr>
2019-03-08 15:27:49 +0100

    case_item, case_clause and rule_case not finished but commented

--- src/parser/rules/key_word/read_rule_case.c
+++ src/parser/rules/key_word/read_rule_case.c
@@ -3,9 +3,23 @@
 bool read_rule_case(struct parser *p)
 {
     unsigned int tmp = p->cursor;
+    return true;
+}
+/*
+	WAITING FOR FINISHING TOUCH
 
-    if (true) {
-        // Code the ast here
+    if (parser_readtext(p, "case")
+        && read_asiignment_word(p)
+	&& ZERO_OR_MANY(parser_readchar(p, '\n'))
+	&& parser_readtext(p, "in")
+	&& ZERO_OR_MANY(parser_readchar(p, '\n'))
+	&& OPTIONAL(read_case_clause(p))
+	&& parser_readtext(p, "esac")) {
+
+        struct ast_node *ast = ast_case_item_init();
+
+        ast_recover_all_from_parser(ast, p, );
+
         return true;
     }
 
@@ -13,3 +27,16 @@ bool read_rule_case(struct parser *p)
 
     return false;
 }
+
+char *ast_rule_case_to_string(struct parser *p)
+{
+    return default_to_string(ast, "rule_case");
+}
+
+struct ast_node *ast_rule_case_init()
+{
+    struct ast_node *ast = ast_init(AST_RULE_CASE, NULL);
+    ast->to_string = ast_rule_case_to_string;
+    return ast;
+}
+*/
