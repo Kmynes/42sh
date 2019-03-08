e1127f7d6e4b2eebec9b352e620dbc52f3eebb5b
yann.moussan <yann.moussan@epita.fr>
2019-03-08 23:19:36 +0100

    case_item case_clause rule_case Yann

--- src/parser/rules/rules.h
+++ src/parser/rules/rules.h
@@ -144,8 +144,10 @@ struct ast_node *ast_do_group_init();
 
 bool read_case_clause(struct parser *p);
 struct ast_case_clause{char *elt;};
+struct ast_node *ast_case_clause_init();
 
 bool read_case_item(struct parser *p);
 struct ast_case_item{char *elt;};
+struct ast_node *ast_case_item_init();
 
 char *default_to_string(struct ast_node *ast, char *type);
