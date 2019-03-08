fecac725f1ba8373f819facbbf0a29aa59412d49
yann.moussan <yann.moussan@epita.fr>
2019-03-08 23:16:12 +0100

    case_item case_clause rule_case Yann

--- src/parser/rules/key_word/read_case_item.c
+++ src/parser/rules/key_word/read_case_item.c
@@ -1,25 +1,30 @@
 #include <parser/rules/rules.h>
 
+bool test_ZERO_OR_MANY_case_item(struct parser *p)
+{
+    while(parser_readchar(p, '|') && read_assignment_word(p));
+
+    return true;
+}
+
 bool read_case_item(struct parser *p)
 {
     unsigned int tmp = p->cursor;
-    if(tmp == p->cursor)
-	    return true;
-    return true;
-}
-/*
-	WAITING FOR FINISHING TOUCH
-
+    
     if (OPTIONAL(parser_readchar(p, '('))
 	&& read_assignment_word(p)
-	&& ZERO_OR_MANY(parser_readchar(p, '|') && read_assignmentt_word(p))
+	&& test_ZERO_OR_MANY_case_item(p)
 	&& parser_readchar(p, ')')
 	&& ZERO_OR_MANY(parser_readchar(p, '\n'))
 	&& read_compound_list(p)) {
         
 	struct ast_node *ast = ast_case_item_init();
 
-	ast_recover_all_from_parser(ast, p, );
+	ast_recover_all_from_parser(ast, p, AST_ASSIGNEMENT_WORD);
+	//test 'ZERO_OR_MANY' de word pour recover ==> ast_recover_all_from_parser(ast, p, AST_ASSIGNMENT_WORD);
+	ast_recover_all_from_parser(ast, p, AST_COMPOUND_LIST);
+
+	ast_set_in_parser(p, ast);
 	
         return true;
     }
@@ -29,7 +34,7 @@ bool read_case_item(struct parser *p)
     return false;
 }
 
-char *ast_case_item_to_string(struct parser *p)
+char *ast_case_item_to_string(struct ast_node *ast)
 {
     return default_to_string(ast, "case_item");
 }
@@ -40,4 +45,3 @@ struct ast_node *ast_case_item_init()
     ast->to_string = ast_case_item_to_string;
     return ast;
 }
-*/
