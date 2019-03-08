ec7a52728a6634aaffaf995cb5bf2c7247b17fdc
yann.moussan <yann.moussan@epita.fr>
2019-03-08 03:28:20 +0100

    setting up rule_case case_item case_clause

--- src/parser/rules/key_word/read_case_clause.c
+++ src/parser/rules/key_word/read_case_clause.c
@@ -13,3 +13,15 @@ bool read_case_clause(struct parser *p)
 
     return false;
 }
+
+char *ast_case_clause_to_string(struct parser *p)
+{
+    return default_to_string(ast, "case_clause");
+}
+
+struct ast_node *ast_rule_while_init()
+{
+    struct ast_node *ast = ast_init(AST_CASE_CLAUSE, NULL);
+    ast->to_string = ast_case_clause_to_string(NULL);
+    return ast;
+}
