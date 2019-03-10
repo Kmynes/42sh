85efe098bb6f771e295c362a7b3dbceaf8f6c2a8
kVoyerDev <kevin.voyer.developpeur@gmail.com>
2019-03-10 13:02:41 +0000

    Modification docker test [kevin]

--- src/parser/ast/ast.h
+++ src/parser/ast/ast.h
@@ -5,11 +5,14 @@
 
 // ast_accessor
 void ast_set_in_parent(struct ast_node *parent, struct ast_node *ast);
-struct ast_node *ast_get_from_parent(struct ast_node *parent, enum ast_node_type type_ast_search);
+struct ast_node *ast_get_from_parent(struct ast_node *parent,
+    enum ast_node_type type_ast_search);
 void ast_set_in_parser(struct parser *p, struct ast_node *ast);
-struct ast_node *ast_get_from_parser(struct parser *p, enum ast_node_type type_ast_search);
-void ast_recover_all_from_parser(struct ast_node *parent, struct parser *p
-                                    , enum ast_node_type type);
+struct ast_node *ast_get_from_parser(struct parser *p,
+    enum ast_node_type type_ast_search);
+void ast_recover_all_from_parser(struct ast_node *parent, struct parser *p,
+    enum ast_node_type type);
+
 // ast_init
 struct ast_node *ast_init(enum ast_node_type type, void *data);
 char *ast_node_default_to_string(struct ast_node *ast);
