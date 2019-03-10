08594e1cdf9109983e8bc346a83240ee0768e061
kVoyerDev <kevin.voyer.developpeur@gmail.com>
2019-03-10 10:19:57 +0000

    Deplacment docker config [kevin] Sun Mar 10 10:19:56 UTC 2019

---
+++ tests/Dockerconfig/scripts/init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "You are in a the docker container"
+
+echo "You can run [make check] for improved 42sh project"
+
+echo "Run [exit] for quit this container"
+
+rm -rf build
+
+/bin/bash
