d2fead44fbeed518f7b1902170514cbe04bf322d
YannMOUSSAN <yann.moussan@epita.fr>
2019-03-14 14:19:43 +0100

    setting up ast_print

---
+++ src/ast_print.c
@@ -0,0 +1,24 @@
+#include "ast_print.h"
+
+int ast_print(struct parser *p)
+{
+    /*
+    FILE *fp = fopen("../ast.dot", "w");
+    char *c;
+
+    if (fp == NULL)
+        return 1;
+
+    while() //boucle permetant de parcourir le parseur et de récup tt les ast a print
+    {
+        case //ici faire un switch case
+    }
+
+    fclose(fp);
+    */
+
+    if (p == NULL)
+        return 1;
+
+    return 0;
+}
