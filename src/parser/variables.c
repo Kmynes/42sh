bd517a199a88ead5386f0a7d537df8b0ae3ecc96
kVoyerDev <kevin.voyer.developpeur@gmail.com>
2019-03-14 16:27:10 +0000

    Correction variables [kevin]

--- src/parser/variables.c
+++ src/parser/variables.c
@@ -4,7 +4,7 @@ struct key_value *variables = NULL;
 
 static struct key_value *create_variable(char *key, char *value)
 {
-    struct key_value *v= malloc(sizeof(struct key_value));
+    struct key_value *v = malloc(sizeof(struct key_value));
     v->key = key;
     v->value = value;
     v->next = NULL;
@@ -18,19 +18,38 @@ void variables_init()
 
 struct key_value *variables_add(char *key, char *value)
 {
+    if (variables == NULL)
+        variables_init();
+
+    if (variables->key == NULL)
+    {
+        variables->key = key;
+        variables->value = value;
+        return variables;
+    }
+
+    struct key_value *kv_exist = variables_update(key, value);
+    if (kv_exist)
+        return kv_exist;
+
     struct key_value *buff = variables;
     while (buff->next)
         buff = buff->next;
 
-    buff->next = create_variable(key, value);    
-    return NULL;
+    buff->next = create_variable(key, value);
+
+    return buff->next;
 }
 
 struct key_value *variables_get(char *key)
 {
+    if (strcmp(key, variables->key) == 0)
+        return variables;
+
     struct key_value *buff = variables;
     struct key_value *found = NULL;
-    while (buff->next && !found)
+
+    while (buff && !found)
     {
         if (strcmp(key, buff->key) == 0)
             found = buff;
@@ -55,35 +74,33 @@ struct key_value *variables_update(char *key, char *value)
 
 void variables_delete(char *key)
 {
+
     struct key_value *current = variables;
     struct key_value *next;
-
-    if (!current->next)
-        return;
-
-    // if variable is at the head of the list
     if (strcmp(current->key, key) == 0)
     {
-        variables = current->next;
-        free(current->key);
-        free(current->value);
-        free(current);
-        variables_delete(key);
+        next = variables->next;
+        free(variables->key);
+        free(variables->value);
+        free(variables);
+        variables = next;
         return;
     }
 
     next = current->next;
 
-    while (next->next)
+    while (current->next)
     {
         if (strcmp(next->key, key) == 0)
         {
+
             current->next = next->next;
+
             free(next->key);
             free(next->value);
             free(next);
-            next = current->next;
-            continue;
+
+            return;
         }
 
         current = next;
