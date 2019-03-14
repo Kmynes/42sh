8ff83b73417f6020c9a40476ce040f05fc3da73b
Daniel Sebton <dsebton@gmail.com>
2019-03-14 18:49:10 +0100

    Doxygen now builds correctly. Python test suite now passes.

--- src/42sh.c
+++ src/42sh.c
@@ -1,15 +1,24 @@
 #include <errors.h>
 #include "parser/parser.h"
 #include "options.h"
+/** \mainpage My Personal Index Page
+ *
+ * \section intro_sec Introduction
+ *
+ * This is the introduction.
+ *
+ * \section install_sec Installation
+ *
+ * \subsection step1 Step 1: Opening the box
+ *
+ * etc...
+ */
 /**
  * \file 42sh.c
- * \brief Programme
+ * \brief This file contains the main file that is called when the executable is run.
  * \author Blueshell42
- * \version 0.3
+ * \version v0.3
  * \date mars 2019
- *
- * 
- *
  */
 int main(int argc, char ** argv)
 {
