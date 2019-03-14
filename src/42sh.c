46771238fd4e8ffae6343cd0e0a36a0431dff257
Daniel Sebton <dsebton@gmail.com>
2019-03-14 19:03:35 +0100

    Doxy now uses readme as a main page

--- src/42sh.c
+++ src/42sh.c
@@ -1,18 +1,6 @@
 #include <errors.h>
 #include "parser/parser.h"
 #include "options.h"
-/** \mainpage My Personal Index Page
- *
- * \section intro_sec Introduction
- *
- * This is the introduction.
- *
- * \section install_sec Installation
- *
- * \subsection step1 Step 1: Opening the box
- *
- * etc...
- */
 /**
  * \file 42sh.c
  * \brief This file contains the main file that is called when the executable is run.
