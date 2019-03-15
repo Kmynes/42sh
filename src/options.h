a3718ce19ed7f388cba393bca829f176a4493a86
kVoyerDev <kevin.voyer.developpeur@gmail.com>
2019-03-15 13:40:30 +0000

    Creation of doc for files [variables] and correction of includes [kevin] Fri Mar 15 13:40:30 UTC 2019

--- src/options.h
+++ src/options.h
@@ -1,8 +1,5 @@
 #pragma once
-
-#include <stddef.h>
 #include <stdbool.h>
-
 
 int options_parser(char **argv, int argc, char **env);
 bool has_options(char *options, char option);
--- src/parser/parser.h
+++ src/parser/parser.h
@@ -1,10 +1,6 @@
 #pragma once
 
-#include <stddef.h>
-#include <stdbool.h>
-#include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <err.h>
