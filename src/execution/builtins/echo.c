d6acf908c8a7e6f5a6ca763371b414853c8a9816
Thibault Lebrun <thibault.lebrun@gitguardian.com>
2019-03-15 18:31:55 +0100

    Add code for handling builtins

--- src/execution/builtins/echo.c
+++ src/execution/builtins/echo.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int builtin_echo(char **args, size_t n)
+int builtin_echo(char **args)
 {
-    if (n == 0)
+    if (args[0] == NULL)
         return 1;
 
     printf("%s", args[0]);
-    for (size_t i = 1; i < n; i++)
+    for (size_t i = 1; args[i]; i++)
         printf("%s ", args[i]);
 
     return 0;
