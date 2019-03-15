fdacb5de504db7460c7614570dbe711061a5d8a2
Thibault Lebrun <thibault.lebrun@gitguardian.com>
2019-03-15 18:31:55 +0100

    Add code for handling builtins

--- src/execution/builtins/builtins.c
+++ src/execution/builtins/builtins.c
@@ -1,3 +1,6 @@
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
 #include "builtins.h"
 
 extern struct builtin *builtin_table;
@@ -5,9 +8,28 @@ extern struct builtin *builtin_table;
 void builtin_table_init(void)
 {
     struct builtin table[] = {
-            {"echo", builtin_echo}
+//            {"echo", builtin_echo},
+            {NULL, NULL}
     };
 
     builtin_table = table;
 }
+
+int (*get_builtin(char *name))(char **args)
+{
+    size_t i = 0;
+    while (true)
+    {
+        struct builtin b = builtin_table[i];
+        if (b.name == NULL)
+            break;
+
+        if (strcmp(b.name, name) == 0)
+            return b.func;
+        i++;
+    }
+
+    return NULL;
+}
+
 
---
+++ tests/unit/execution/builtins/builtins.c
@@ -0,0 +1,12 @@
+#include <unit/execution/test_execution.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <execution/builtins/builtins.h>
+#include <stdio.h>
+
+extern struct builtin *builtin_table;
+void test_get_builtin(void)
+{
+//    builtin_table_init();
+//    assert(get_builtin("echo"));
+}
