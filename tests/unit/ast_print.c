fe5b9c70198115d9735d16f7e723c61a522150d6
Thibault Lebrun <thibault.lebrun@gitguardian.com>
2019-03-16 11:54:50 +0100

    correct bug ast_print test

--- tests/unit/ast_print.c
+++ tests/unit/ast_print.c
@@ -1,11 +1,15 @@
 #include <assert.h>
 #include <stdbool.h>
 #include <parser/parser.h>
+#include <parser/rules/rules.h>
 
 void test_ast_print(void)
 {
     char *input = "echo toto";
     struct parser *p = parser_new_from_string(input);
+    read_input(p);
     struct ast_node *ast = p->ast->children[0];
+//    ast_default_print(ast);
     ast_print(ast, NULL);
+    parser_free(p);
 }
