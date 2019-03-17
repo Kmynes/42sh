23927bde5657a0a7e72ecbb86717df3395331f80
kVoyerDev <kevin.voyer.developpeur@gmail.com>
2019-03-17 13:46:59 +0000

    implement new test for [var_interpret_quote]

--- tests/sh_test_files/var_interpret_quote.sh
+++ tests/sh_test_files/var_interpret_quote.sh
@@ -1,12 +1,18 @@
 dir_test='tests'
 dir_sh_test='sh_test_files'
-printer='printer.py'
+printer_file='printer.py'
 
-path="${dir_test}/${dir_sh_test}/${printer}"
+path_printer="${dir_test}/${dir_sh_test}/${printer_file}"
 
 suffix='team'
 part1='My name is'
 part2='BlueShell 42'
 phrase="${part1} $part2 ${suffix}"
 
-python3 "${path}" $phrase
+python3 $path_printer $phrase
+
+prog='python3'
+
+$prog --version
+
+$prog $path_printer $phrase
