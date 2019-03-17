db0d7d07309c63592d1a10fb6a5be133d64ec044
Daniel Sebton <dsebton@gmail.com>
2019-03-17 08:56:03 +0100

    Added doxygen doc

--- src/42sh.c
+++ src/42sh.c
@@ -5,6 +5,9 @@
  * \file 42sh.c
  * \brief This file contains the main file that is called when the executable
  * is run.
+ * 
+ * It passes the argc and argv command line arguments to the
+ * options_parser.
  * \author Blueshell42
  * \version v0.3
  * \date mars 2019
