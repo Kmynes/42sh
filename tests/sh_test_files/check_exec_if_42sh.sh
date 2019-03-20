d25863bbaaa8db4b7e24c8d02ae632d3b0f28471
kVoyerDev <kevin.voyer.developpeur@gmail.com>
2019-03-20 08:46:42 +0000

    Use echo for print

--- tests/sh_test_files/check_exec_if_42sh.sh
+++ tests/sh_test_files/check_exec_if_42sh.sh
@@ -1,7 +1,1 @@
-path='../tests/sh_test_files'
-prg="python3 ${path}"
-prg_err="$prg/error.py"
-prg_suc="$prg/succes.py"
-printer="$prg/printer.py"
-
-$printer 'success'
+echo 'success'
--- tests/sh_test_files/check_exec_if_bash.sh
+++ tests/sh_test_files/check_exec_if_bash.sh
@@ -2,7 +2,6 @@ path='../tests/sh_test_files'
 prg="python3 ${path}"
 prg_err="$prg/error.py"
 prg_suc="$prg/succes.py"
-printer="$prg/printer.py"
 
-if $prg_err; then $printer 'error'; fi
-if $prg_suc; then $printer 'success'; fi
+if $prg_err; then echo 'error'; fi
+if $prg_suc; then echo 'success'; fi
