5b832fba1dad27ae30ac936f0de4e44c3c1c7447
YannMOUSSAN <yann.moussan@epita.fr>
2019-03-22 21:17:56 +0100

    history almost done changes on prompt for testing

---
+++ src/42sh_history.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include "./parser/parser.h"
+
+void put_in_history_file();
+
+void read_history();
+
+void erease_tmp_history();
