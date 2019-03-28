08e94ad3f056ed50a11de453e5cac030ffb9a7ea
Daniel Sebton <daniel.sebton@epita.fr>
2019-03-28 09:59:14 +0100

    tests: added "for" tests

---
+++ tests/sh_test_files/for/break_basic.sh
@@ -0,0 +1,5 @@
+for number in 1 2 3 4 5
+do 
+    echo $number
+    break
+done
---
+++ tests/sh_test_files/for/continue_complex.sh
@@ -0,0 +1,8 @@
+for number in 1 2 3 4 5
+do
+    for number in 1 2 3
+    do
+        echo $number
+        continue 2
+    done
+done
