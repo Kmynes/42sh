0669b0045e46c4efcf79af616225afafc0cf1d0d
Daniel-Epita <daniel.sebton@epita.fr>
2019-04-02 23:02:54 +0200

    tests: finally fixed logic tests

--- tests/sh_test_files/logic/greater_than.sh
+++ tests/sh_test_files/logic/greater_than.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-if [[1 -gt 2]]
+if [[ 1 -gt 2 ]]
 then
     echo "KO"
 fi
 
-if [[2 -gt 2]]
+if [[ 2 -gt 2 ]]
 then
     echo "KO"
 fi
 
-if [[3 -gt 2]]
+if [[ 3 -gt 2 ]]
 then
     echo "OK"
 fi
--- tests/sh_test_files/logic/less_than.sh
+++ tests/sh_test_files/logic/less_than.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-if [[1 -lt 2]]
+if [[ 1 -lt 2 ]]
 then
     echo "OK"
 fi
 
-if [[2 -lt 2]]
+if [[ 2 -lt 2 ]]
 then
     echo "KO"
 fi
 
-if [[3 -lt 2]]
+if [[ 3 -lt 2 ]]
 then
     echo "KO"
 fi
